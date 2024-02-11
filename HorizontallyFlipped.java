6a5547db8bc8e3a46b32cba30f6b6816769834b0
KRmiyamoto <miyamoto@grinnell.edu>
2024-02-10 22:25:54 -0600

    VerticallyFlipped + HorizontallyFlipped full drafts

--- HorizontallyFlipped.java
+++ HorizontallyFlipped.java
@@ -1,3 +1,52 @@
-public class HorizontallyFlipped {
-  
-}
+/**
+ * Given a text block, rearranges horizontally.
+ */
+public class HorizontallyFlipped implements TextBlock {
+  // Fields
+  /**
+   * The contents to be flipped.
+   */
+  TextBlock contents;
+
+  // Constructor
+  /**
+   * Build a Centered TextBlock of given width and contents.
+   */
+  public HorizontallyFlipped(TextBlock _contents) {
+    this.contents = _contents;
+  } // HorizontallyFlipped(String)
+
+  // Methods
+  /**
+   * Get one row from the block.
+   *
+   * @pre 0 <= i < this.height()
+   * @exception Exception if the preconditions are not met
+   */
+  public String row(int i) throws Exception {
+    if ((i < 0) || (i >= this.height())) {
+       throw new Exception("Invalid row " + i);
+     } else {
+      String initial = this.contents.row(i);
+      String flipped = "";
+      for (int j = 0; j < initial.length(); j++) {
+        flipped = initial.charAt(j) + flipped;
+      } // for
+      return flipped;
+    } 
+  } // row(int)
+
+  /**
+   * Determine how many rows are in the block.
+   */
+  public int height() {
+    return this.contents.height();
+  } // height()
+
+  /**
+   * Determine how many columns are in the block.
+   */
+  public int width() {
+    return this.contents.width();
+  } // width()
+} // HorizontallyFlipped
--- VerticallyFlipped.java
+++ VerticallyFlipped.java
@@ -1,3 +1,48 @@
-public class VerticallyFlipped {
-  
-}
+/**
+ * Given a text block, rearranges vertically.
+ */
+public class VerticallyFlipped implements TextBlock {
+  // Fields
+  /**
+   * The contents to be flipped.
+   */
+  TextBlock contents;
+
+  // Constructor
+  /**
+   * Build a Centered TextBlock of given width and contents.
+   */
+  public VerticallyFlipped(TextBlock _contents) {
+    this.contents = _contents;
+  } // VerticallyFlipped(String)
+
+  // Methods
+  /**
+   * Get one row from the block.
+   *
+   * @pre 0 <= i < this.height()
+   * @exception Exception if the preconditions are not met
+   */
+  public String row(int i) throws Exception {
+    if ((i < 0) || (i >= this.height())) {
+       throw new Exception("Invalid row " + i);
+     } else {
+      return this.contents.row(this.height() - (i + 1));
+    } 
+  } // row(int)
+
+  /**
+   * Determine how many rows are in the block.
+   */
+  public int height() {
+    return this.contents.height();
+  } // height()
+
+  /**
+   * Determine how many columns are in the block.
+   */
+  public int width() {
+    return this.contents.width();
+  } // width()
+
+} // VerticallyFlipped
